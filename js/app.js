1b0be89d528b8d1fbe4b50e76b1e96c7f2e5c952
Phil <phil@htglearn.com>
2018-09-11 11:17:33 -0400

    Improved formatting

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 // Pixel Maker JavaScript App for Udacity Course
 
-// selects the button, defines a variable to represent the button then adds an event listener
-var myButton = document.querySelector('#mySubmitButton');
+// selects the button, defines variable for the button, adds an event listener
+const myButton = document.querySelector('#mySubmitButton');
 myButton.addEventListener('click',function(){makeGrid()});
 
-// selects the grid, defines a variable to represent the grid then adds an event listener looking for a click
+// selects the grid, defines a variable, adds an event listener
 // executes the function "respondtoTheClick" when any element on the grid is clicked
 const myGrid2 = document.querySelector('#pixelCanvas');
 myGrid2.addEventListener('click',function(event){respondToTheClick(event),false});
 
-// this function checks to ensure the element clicked is a cell not a row in the table then sets the style.background color
+// function checks to ensure the element clicked is a cell, sets the style.background color
 function respondToTheClick(a) {
     if (a.target.nodeName === 'TD'){
         a.target.style.backgroundColor=myColor.value;
@@ -22,18 +22,21 @@ const myColor = document.querySelector('#colorPicker');
 
 
 function makeGrid() {
-    var myRows = document.querySelector('#inputHeight');
-    var myColumns = document.querySelector('#inputWidth');
-    var gridString = "";
-    const newRow = '<tr>'
-    const newColumn = '<td></td>'
-    const endRow = '</tr>'
+    // Initializing variables used in this function
+    const myRows = document.querySelector('#inputHeight');
+    const myColumns = document.querySelector('#inputWidth');
+    let gridString = "";
+    const newRow = '<tr>';
+    const newColumn = '<td></td>';
+    const endRow = '</tr>';
     
     // select pixelCanvas element from document
     const myGrid = document.querySelector('#pixelCanvas');
     
     // reset number of rows and columns to prepare for new size
     myGrid.innerHTML = '';
+    
+    // create new grid with nested for loops
     for (x=0; x < myRows.value; x++) {
         gridString = gridString +newRow;
 
