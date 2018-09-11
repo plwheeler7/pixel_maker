f756b8e597c983c061b76d6e14cb18b01eaf4611
Phil <phil@htglearn.com>
2018-09-11 10:13:53 -0400

    Added Color Picker and Size

--- js/app.js
+++ js/app.js
@@ -1,12 +1,43 @@
 // Pixel Maker JavaScript App for Udacity Course
 
-/ Select color input
+// Select color input
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
+var myButton = document.querySelector('#mySubmitButton');
+myButton.addEventListener('click',function(){makeGrid()});
+
+const myGrid2 = document.querySelector('#pixelCanvas');
+myGrid2.addEventListener('click',function(event){respondToTheClick(event),false});
+
+
+function respondToTheClick(a) {
+    if (a.target.nodeName === 'TD'){
+        a.target.style.backgroundColor=myColor.value;
+    }
+    else console.log(a.target.nodeName, myColor.value);
+}
+
+const myColor = document.querySelector('#colorPicker');
+
 
 function makeGrid() {
+    var myRows = document.querySelector('#inputHeight');
+    var myColumns = document.querySelector('#inputWidth');
+    var gridString = "";
+    const newRow = '<tr>'
+    const newColumn = '<td></td>'
+    const endRow = '</tr>'
+    // select pixelCanvas element from document
+    const myGrid = document.querySelector('#pixelCanvas');
+    // reset number of rows and columns to prepare for append
+    myGrid.innerHTML = '';
+    for (x=0; x < myRows.value; x++) {
+        gridString = gridString +newRow;
 
-// Your code goes here!
-
+        for (y=0; y < myColumns.value; y++) {
+            gridString = gridString +newColumn;
+        }
+        gridString = gridString +endRow;
+    }
+    myGrid.innerHTML = gridString;
 }
